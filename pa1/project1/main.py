f1413acb88732e18cd208c4f5149d08b11e10719
eb4361 <emilbatakkiev@gmail.com>
2021-03-06 13:56:58 +0100

    difficult start

--- pa1/project1/main.py
+++ pa1/project1/main.py
@@ -1,1 +1,15 @@
-print("start")
+import urllib.request
+from bs4 import BeautifulSoup
+
+f = urllib.request.urlopen('http://www.gov.si')
+
+page = f.read().decode('utf-8')
+
+links = ['http://www.gov.si', 'http://www.evem.gov.si', 'http://www.e-uprava.gov.si', 'http://www.e-prostor.gov.si']
+
+soup = BeautifulSoup(page)
+
+linkovi = soup.find_all('a', href=True)
+
+for lnk in linkovi[1:10]:
+    print(lnk)
